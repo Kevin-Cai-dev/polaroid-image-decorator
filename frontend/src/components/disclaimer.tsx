137a969604a89910c4dce009df5ffda95eac82c9
Kevin Cai <kevin.cai1028@gmail.com>
2022-09-26 19:11:36 +1000

    added JPEG notice

--- frontend/src/components/disclaimer.tsx
+++ frontend/src/components/disclaimer.tsx
@@ -1,9 +1,31 @@
+import { ReactNode } from 'react';
+
+interface Props {
+    children: ReactNode;
+}
+
+const DisclaimerMessage = ({ children }: Props) => {
+    return <p className="m-0">{children}</p>;
+};
+
 export const Disclaimer = () => {
-    return (
-        <p className="border-l-4 border-base-300 pl-4 italic">
+    const message = (
+        <DisclaimerMessage>
             {
                 "✨ If you run into a fetch error, most likely the backend isn't running ¯\\_(ツ)_/¯"
             }
-        </p>
+        </DisclaimerMessage>
+    );
+    const returnFormat = (
+        <DisclaimerMessage>
+            Note that images are returned in <code>JPEG</code> format
+        </DisclaimerMessage>
+    );
+
+    return (
+        <div className="mb-4 border-l-4 border-accent pl-4 italic">
+            {message}
+            {returnFormat}
+        </div>
     );
 };
