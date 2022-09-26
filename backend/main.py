5f1b90b08e9b0f61e4c88071c3a8acc54bb2004f
Kevin Cai <kevin.cai1028@gmail.com>
2022-09-26 15:22:18 +1000

    updated origin list

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,16 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import os
+
 from routers import images
+
+load_dotenv()
+
+origins = [os.getenv("PRODUCTION_URL"), os.getenv("LOCAL_URL")]
 
 app = FastAPI()
 app.include_router(images.router)
-
-origins = ["*"]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
